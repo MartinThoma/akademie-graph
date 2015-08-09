9c2e803a5a016b5041da0ac133f358e39d24899a
der_herr_g <gebhard.mgg@googlemail.com>
2015-08-10 00:54:17 +0200

    Added course names

--- insert/index.php
+++ insert/index.php
@@ -114,14 +114,18 @@ if(isset($_POST['me'])) {
 $data = get_people_data_by_course();
 $adjacency = get_adjacency();
 
-$course_list = "Test"
+$course_list = array("1" => "Kausalität, Exoplaneten und Black Jack: Wie man aus Daten lernt",
+                     "3" => "Britische Ideengeschichte und deutsche Begriffsgeschichte im Vergleich und in der Anwendung",
+                     "4" => "Bin ich zu komplex? Systemtheoretische Ansätze in Forschung und Praxis der Psychologie",
+                     "5" => "Personalized Medicine, Big Data, and the Future of Health Care",
+                     "6" => "Regulierung und Überwachung des Internets – Ende der Kommunikationsfreiheiten?",
+                     "7" => "TTIP – Transatlantic Trade and Investment Partnership")
 
 echo $twig->render('insert.twig', array('heading' => $heading,
                                         'file'=> "insert",
                                         'msg' => $msg,
                                         'data' => $data,
                                         'adjacency' => $adjacency,
-                                        'me' => $me,
-                                        'course_list' => $course_list
+                                        'me' => $me
                                        )
                   );
